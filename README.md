929ccc0aca1afaf6bc0e40afe6f75072cee7a721
Ionuț Arțăriși <iartarisi@suse.cz>
2014-02-25 17:29:45 +0100

    port to havana

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a proof of concept to allow LDAP authentication while using the SQL backend for all the usual operations. No users or groups are copied from LDAP. LDAP users are assigned a default role and tenant when they first login if they don't already have one (user_project_metadata table). For granting roles to users (`keystone user-role-add`), only the user id from LDAP is inserted into the SQL backend.
 
-The code in this branch has only been tested on the stable/grizzly branch of openstack keystone!
+The code in this branch has only been tested on the stable/havana branch of openstack keystone!
 
 * * *
 
@@ -11,22 +11,6 @@ The code in this branch has only been tested on the stable/grizzly branch of ope
 Since this backend relies on both the LDAP and SQL backends, you have to configure both beforehand. Use the usual configuration options found in /etc/keystone/keystone.conf. However, from the LDAP backend's config, only the ldap.user* options will be used by the hybrid backend (so no tenant/role options).
 
 You should try to see that user authentication works fine with the LDAP backend before trying on the hybrid backend. Also make sure that `keystone user-list` works using the LDAP identity backend.
-
-Remove the database Foreign Key constraint which disallows user_id values which are not present in the SQL backend:
-
-```SQL
-$ pgsql keystone -U keystone -W
-ALTER TABLE user_project_metadata DROP CONSTRAINT user_project_metadata_user_id_fkey;
-```
-
-or for MySQL:
-
-```SQL
-$ mysql keystone
-SHOW CREATE TABLE user_project_metadata;
--- look for the constraint name referencing user.id
-ALTER TABLE user_project_metadata DROP FOREIGN KEY user_project_metadata_ibfk_1;
-```
 
 Set the identity backend to `hybrid` (it will use both the LDAP and the SQL backends under the hood):
 
@@ -39,7 +23,7 @@ Copy the `hybrid.py` file to the `keystone/identity/backends/` folder of your in
 
 Restart keystone.
 
-Now you can assign custom roles to users in LDAP. Make user you use the LDAP user-id returned by the `keystone user-list` query.
+Now you can assign custom roles to users in LDAP. Make user you use one of the LDAP user-ids returned by the `keystone user-list` query.
 
 ```
 keystone user-role-add --user-id=12345 --role-id <role-id> --tenant-id <tenant-id>
--- hybrid.py
+++ hybrid.py
@@ -20,40 +20,42 @@
 
 from keystone import config
 from keystone import exception
-from keystone.common import sql as keystone_sql
 from keystone.common import utils
-from keystone.common import logging
 from keystone import identity
 from keystone.identity.backends import ldap as ldap_backend
 from keystone.identity.backends import sql
+from keystone.openstack.common import log
+from keystone.openstack.gettextutils import _
 
 DEFAULT_TENANT = 'default_tenant'
 DEFAULT_DOMAIN = 'default'
 DEFAULT_ROLE = 'default_role'
 CONF = config.CONF
-LOG = logging.getLogger(__name__)
+LOG = log.getLogger(__name__)
+
 
 class Identity(sql.Identity):
     def __init__(self, *args, **kwargs):
         super(Identity, self).__init__(*args, **kwargs)
         self.user = ldap_backend.UserApi(CONF)
-        
-    # Identity interface
-    def authenticate(self, user_id=None, tenant_id=None, password=None):
-        """Authenticate based on a user, tenant and password.
 
-        Expects the user object to have a password field and the tenant to be
-        in the list of tenants on the user.
+    # Identity interface
+    def authenticate(self, user_id, password):
+        """Authenticate based on a user and password.
+
+        Tries to authenticate using the SQL backend first, if that fails
+        it tries the LDAP backend.
 
         """
+        session = self.get_session()
         try:
-            user_ref = self._get_user(user_id)
+            user_ref = self._get_user(session, user_id)
         except exception.UserNotFound:
             raise AssertionError('Invalid user / password')
 
-        ldap_user = False
         # if the user_ref has a password, it's from the SQL backend and
         # we can just check if it coincides with the one we got
+        conn = None
         try:
             assert utils.check_password(password, user_ref['password'])
         except TypeError:
@@ -61,67 +63,31 @@ class Identity(sql.Identity):
         except KeyError:  # if it doesn't have a password, it must be LDAP
             try:
                 # get_connection does a bind for us which checks the password
-                assert self.user.get_connection(self.user._id_to_dn(user_id),
+                conn = self.user.get_connection(self.user._id_to_dn(user_id),
                                                 password)
+                assert conn
             except Exception:
                 raise AssertionError('Invalid user / password')
-            ldap_user = True
+            finally:
+                if conn:
+                    conn.unbind_s()
 
-        tenants = self.get_projects_for_user(user_id)
-        if tenant_id and tenant_id not in tenants:
-            raise AssertionError('Invalid tenant')
+        return _set_default_domain(identity.filter_user(user_ref))
 
-        try:
-            tenant_ref = self.get_project(tenant_id)
-            # if the tenant was not found, then there will be no metadata either
-            metadata_ref = self.get_metadata(user_id, tenant_id)
-        except exception.ProjectNotFound:
-            tenant_ref = None
-            metadata_ref = {}
-        except exception.MetadataNotFound:
-            if ldap_user:
-                # if the metadata does not exist and this is an LDAP user we
-                # assign it a default tenant and role
-                default_tenant_id = self.get_project_by_name(
-                    DEFAULT_TENANT, DEFAULT_DOMAIN)['id']
-                default_tenant_id = default
-                role_id = self._get_role_id(DEFAULT_ROLE)
-                self.add_role_to_user_and_project(user_id, default_tenant_id, role_id)
-                metadata_ref = self.get_metadata(user_id, default_tenant_id)
-                LOG.debug("Added the %s role to user %s with default_tenant %s." %
-                          (role_id, user_id, default_tenant_id))
-            else:
-                metadata_ref = {}
-
-        user_ref = _set_default_domain(identity.filter_user(user_ref))
-        return (user_ref, tenant_ref, metadata_ref)
-
-    def _get_role_id(self, role_name):
-        session = self.get_session()
-        query = session.query(sql.Role).filter_by(name=role_name)
-        try:
-            role_ref = query.one()
-        except keystone_sql.NotFound:
-            raise exception.RoleNotFound(role_id=role_name)
-
-        return role_ref.to_dict()['id']
-
-    def _get_user(self, user_id):
+    def _get_user(self, session, user_id):
         # try SQL first
         try:
-            user = super(Identity, self)._get_user(user_id)
+            user_ref = super(Identity, self)._get_user(session, user_id)
         except exception.UserNotFound:
-            pass
+            # then try LDAP
+            return _set_default_domain(self.user.get(user_id))
         else:
-            return user
-
-        # then try LDAP
-        return self.user.get(user_id)
+            return user_ref.to_dict()
 
     def get_user(self, user_id):
         LOG.debug("Called get_user %s" % user_id)
-        user_ref = identity.filter_user(self._get_user(user_id))
-        return _set_default_domain(user_ref)
+        session = self.get_session()
+        return identity.filter_user(self._get_user(session, user_id))
 
     def get_user_by_name(self, user_name, domain_id):
         LOG.debug("Called get_user_by_name %s, %s" % (user_name, domain_id))
@@ -129,43 +95,34 @@ class Identity(sql.Identity):
         try:
             user = super(Identity, self).get_user_by_name(user_name, domain_id)
         except exception.UserNotFound:
-            pass
+            # then try LDAP
+            user_ref = identity.filter_user(self.user.get_by_name(user_name))
+            _validate_default_domain_id(domain_id)
+            return _set_default_domain(user_ref)
         else:
             return user
 
-        # then try LDAP
-        _validate_domain_id(domain_id)
-        ref = identity.filter_user(self.user.get_by_name(user_name))
-        return _set_default_domain(ref)
-
     def list_users(self):
         sql_users = super(Identity, self).list_users()
-        ldap_users = _set_default_domain(self.user.get_all())
+        ldap_users = self.user.get_all_filtered()
         return sql_users + ldap_users
 
-    def get_projects_for_user(self, user_id):
-        project_ids = super(Identity, self).get_projects_for_user(user_id)
-        default_project = self.get_project_by_name(DEFAULT_TENANT, DEFAULT_DOMAIN)
-        project_ids.append(default_project['id'])
-        LOG.debug("get_projects_for_user returns %s" % project_ids)
 
-        return list(set(project_ids))
-
-
-def _validate_domain_id(domain_id):
-    """Validate that the domain ID specified belongs to the default domain.
-
-    """
-    if domain_id != CONF.identity.default_domain_id:
-        raise exception.DomainNotFound(domain_id=domain_id)
-
+# copied from keystone.assignment.core
 def _set_default_domain(ref):
-    """Overrides any domain reference with the default domain."""
+    """If the domain ID has not been set, set it to the default."""
     if isinstance(ref, dict):
-        ref = ref.copy()
-        ref['domain_id'] = CONF.identity.default_domain_id
+        if 'domain_id' not in ref:
+            ref = ref.copy()
+            ref['domain_id'] = CONF.identity.default_domain_id
         return ref
     elif isinstance(ref, list):
         return [_set_default_domain(x) for x in ref]
     else:
         raise ValueError(_('Expected dict or list: %s') % type(ref))
+
+
+def _validate_default_domain_id(domain_id):
+    """Validate that the domain ID specified belongs to the default domain."""
+    if domain_id != CONF.identity.default_domain_id:
+        raise exception.DomainNotFound(domain_id=domain_id)

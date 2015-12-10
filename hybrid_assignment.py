43137f35e2b9ec0c88dbaf14d67f7f6f0346ffc1
Ralf Haferkamp <rhafer@suse.de>
2015-12-10 12:23:38 +0100

    Fix oslo import for liberty

--- hybrid_assignment.py
+++ hybrid_assignment.py
@@ -13,7 +13,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from oslo.config import cfg
+from oslo_config import cfg
 from keystone import config
 from keystone.assignment.backends import sql as sql_assign
 from keystone.assignment.role_backends import sql as sql_role
